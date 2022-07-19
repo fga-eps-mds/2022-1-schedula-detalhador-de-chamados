4f3bd958691e38b8b5a18823b192562cfad32a4f
Matheus Gabriel <matheuscoding@gmail.com>
2022-07-18 23:19:57 -0300

    feat: cria template do microserviço

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Esperando o banco"
+
+while ! nc -z db 5432; do 
+        sleep 0.1
+done
+
+echo "Banco foi inicializado"
+
+echo "inicializado Aplicação"
+
+uvicorn main:app --host 0.0.0.0 --port 5000
