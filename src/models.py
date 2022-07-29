cd3239707c5281a0c5dd32b3a827235ab4f083f0
Nicolas <nszchagas@gmail.com>
2022-07-28 22:06:32 -0300

    [WIP] Model and create/get endpoints.

--- src/models.py
+++ src/models.py
@@ -1,17 +1,20 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
-
+from datetime import timezone
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, TIMESTAMP
+from sqlalchemy.sql import func
 from database import Base
 
 
 class Category(Base):
     __tablename__ = "category"
-    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(250), nullable=False)
     description = Column(String(250))
     active = Column(Boolean, default=True)
-    # relacionado a server default (server_default=)
-    updated_at = Column(DateTime(timezone=True), server_default=datetime.now)
+    updated_at = Column(
+        TIMESTAMP,
+        server_default=func.current_timestamp(),
+        server_onupdate=func.current_timestamp(timezone.utc)
+    )
 
 
 class Problem(Base):
@@ -20,5 +23,9 @@ class Problem(Base):
     name = Column(String(250), nullable=False)
     description = Column(String(250), nullable=False)
     active = Column(Boolean, nullable=False, default=True)
-    updated_at = Column(DateTime(timezone=True), server_default=datetime.now)
+    updated_at = Column(
+        TIMESTAMP,
+        server_default=func.current_timestamp(),
+        server_onupdate=func.current_timestamp(timezone.utc)
+    )
     category_id = Column(Integer, ForeignKey(Category.id), primary_key=True)
--- src/routers/category.py
+++ src/routers/category.py
@@ -1,10 +1,12 @@
-from datetime import datetime
-from fastapi import APIRouter, Depends
-from pydantic import BaseModel
-from database import engine, SessionLocal
-import models
-from sqlalchemy.orm import Session
+import json
 
+from fastapi import APIRouter, Depends, status
+from fastapi.openapi.models import Response
+from pydantic import BaseModel
+
+from src.database import engine, SessionLocal
+import src.models as models
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -18,11 +20,9 @@ def get_db():
 
 
 class CategoryModel(BaseModel):
-    id: int
     name: str
     description: str
     active: bool
-    updated_at: str
 
     class Config:
         schema_extra = {
@@ -37,25 +37,44 @@ class CategoryModel(BaseModel):
 models.Base.metadata.create_all(bind=engine)
 
 
-# @router.get("/categoria/", tags=["Chamado"])
-# def get_chamado(db: Session = Depends(get_db)):
-#     all_data = db.query(models.Category).all()
-#     return{
-#         "message": "Dados buscados com sucesso",
-#         "error": None,
-#         "data": all_data,
-#     }
+@router.get("/categoria/", tags=["Chamado"])
+def get_category(db: Session = Depends(get_db)):
+    try:
+        all_data = db.query(models.Category).all()
+        response_data = {
+            "message": "Dados buscados com sucesso",
+            "error": None,
+            "data": all_data,
+        }
+        return Response(content=response_data, status_code=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        response_data = {
+            "message": "Erro ao buscar dados",
+            "error": str(e),
+            "data": None
+        }
+        return Response(content=response_data, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# @router.post("/categoria/", tags=["Chamado"])
-# def post_chamado(data: CategoryModel, db: Session = Depends(get_db)):
-#     new_object = models.Category(**data.dict())
-
-#     db.add(new_object)
-#     db.commit()
-#     db.refresh(new_object)
-#     return{
-#         "message": "Dados buscados com sucesso",
-#         "error": None,
-#         "data": new_object,
-#     }
+@router.post("/categoria/", tags=["Chamado"])
+def post_chamado(data: CategoryModel, db: Session = Depends(get_db)):
+    try:
+        new_object = models.Category(**data.dict())
+        db.add(new_object)
+        db.commit()
+        db.refresh(new_object)
+        new_object = json.loads(json.dumps(new_object))
+        response_data = {
+            "message": "Dado cadastrado com sucesso",
+            "error": None,
+            "data": new_object
+        }
+        return Response(content=response_data, status_code=status.HTTP_200_OK)
+    except Exception as e:
+        response_data = {
+            "message": "Erro ao buscar dados",
+            "error": str(e),
+            "data": None
+        }
+        return Response(content=response_data, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
