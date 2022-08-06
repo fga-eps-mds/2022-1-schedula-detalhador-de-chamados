6a25c5a7809f72e3843bb9096686060cc60add53
matheus <matheuscoding@gmail.com>
2022-08-06 10:28:09 -0300

    feat: adiciona CORS

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import category, problem
 
@@ -6,6 +9,21 @@ app = FastAPI()
 
 app.include_router(problem.router)
 app.include_router(category.router)
+
+APP_PORT = os.environ.get('APP_PORT', '5000')
+
+origins = [
+    f"http://0.0.0.0:{APP_PORT}",
+    f"http://localhost:{APP_PORT}",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
