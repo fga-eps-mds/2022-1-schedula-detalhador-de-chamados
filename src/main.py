b96e537787e2d98b8a77579d05cf142335c4f606
Matheus Gabriel <matheuscoding@gmail.com>
2022-08-15 12:13:28 -0300

    fix: atualiza CORs

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from routers import category, problem
 
 app = FastAPI()
 
-app.include_router(problem.router)
-app.include_router(category.router)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(problem.router)
+app.include_router(category.router)
 
 
 @app.get("/")
