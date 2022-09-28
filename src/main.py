f2ab452e52e7783924aa085094bca053ac08e4d2
matheus <matheuscoding@gmail.com>
2022-09-28 20:27:44 -0300

    feat: Adiciona nova variavel de ambiente para conficao do cors

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
@@ -11,12 +13,11 @@ app.include_router(request.router)
 app.include_router(problem.router)
 app.include_router(category.router)
 
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://2022-1-schedula-front-homol.vercel.app",
-        "http://localhost:3000",
-    ],
+    allow_origins=[FRONTEND_URL],
     allow_methods=["*"],
     allow_credentials=True,
     allow_headers=["*"],
@@ -29,25 +30,25 @@ async def process_request_headers(request: Request, call_next):
     method = str(request.method)
     url = str(request.url)
 
-    if method == 'GET':
-        if 'chamado' in url or 'problema' in url:
-            if auth not in ['admin', 'manager', 'basic', 'public']:
+    if method == "GET":
+        if "chamado" in url or "problema" in url:
+            if auth not in ["admin", "manager", "basic", "public"]:
                 return response_unauthorized
 
-    if method == 'DELETE':
-        if auth != 'admin':
+    if method == "DELETE":
+        if auth != "admin":
             return response_unauthorized
 
-    if method == 'POST':
-        if 'chamado' in url:
-            if auth not in ['admin', 'manager', 'basic', 'public']:
+    if method == "POST":
+        if "chamado" in url:
+            if auth not in ["admin", "manager", "basic", "public"]:
                 return response_unauthorized
-        elif 'problema' in url or 'categoria' in url:
-            if auth not in ['admin', 'manager']:
+        elif "problema" in url or "categoria" in url:
+            if auth not in ["admin", "manager"]:
                 return response_unauthorized
 
-    if method == 'PUT':
-        if auth not in ['admin', 'manager']:
+    if method == "PUT":
+        if auth not in ["admin", "manager"]:
             return response_unauthorized
 
     return await call_next(request)
@@ -62,9 +63,11 @@ def root():
     return {"APP": "Detalhador de chamados is running"}
 
 
-response_unauthorized = JSONResponse({
-    "message": "Acesso negado",
-    "error": True,
-    "data": None,
-
-}, status.HTTP_401_UNAUTHORIZED)
+response_unauthorized = JSONResponse(
+    {
+        "message": "Acesso negado",
+        "error": True,
+        "data": None,
+    },
+    status.HTTP_401_UNAUTHORIZED,
+)
